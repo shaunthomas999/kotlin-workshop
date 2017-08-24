03d7a4c36f3160c4e07eea6521bcb8585e351220
Shaun Thomas <shaunthomas999@gmail.com>
2017-08-24 16:16:48 +0200

    Updated NormalFunctions Kotlin file

--- src/eFunctions/ANormalFunctions.kt
+++ src/eFunctions/ANormalFunctions.kt
@@ -8,6 +8,10 @@ fun max2(a: Int, b: Int) = if (a < b) a else b
 
 fun sum(a: Int, b: Int, c: Int = 0) = a + b + c // // default
 
+fun printOne(element: Int) { // Default return type Unit
+    println(element)
+}
+
 fun printMany(vararg elements: Int) { // vararg
     for (element in elements) {
         println(element)
@@ -22,12 +26,26 @@ fun doSomething(vararg elements: Int) {
     }
 }
 
+fun doSomeOtherThing(param: String?) { // ? nullable
+
+    // Safe calls (?) Dealing with nullable objects.
+    // !! - Exempt Intellij IDEA from showing error
+    println("Length of param > ${param?.length}")
+}
+
+
 fun main(args: Array<String>) {
     sum(1, 2)
     sum(1, 2, 3)
 
     sum(b = 2, a = 1) // named arguments
 
+    printOne(1)
+
     printMany(1, 2, 3, 4, 5)
+
+    doSomeOtherThing(null)
+    doSomeOtherThing("Sytac")
+
 }
 
